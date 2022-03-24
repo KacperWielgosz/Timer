87058fc2dbdeb473042f310c25a1f5b0698c3258
K W <k4cperwi3lgosz@gmail.com>
2022-03-24 14:50:47 +0100

    Stop count not working, cant add styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import Button from './components/Button/Button'
 import React, {useEffect, useState} from "react";
 import Time from './components/Time/Time'
-
-
-  let counter = null;
+//dlaczego counter musi być poza obiektem
+let counter = null;
 
 const App = () => {
 
   const [time, setTime] = useState(0);
-  const [count, setCount] = useState(0);
-
   const [start, setStart] = useState(false);
+
+  const [count, setCount] = useState(0);
   const [countdown, setCountdown] = useState(false);
-
-
 
   useEffect(() => {
     let timer = null;
@@ -25,48 +22,58 @@ const App = () => {
     } else {
       clearInterval(timer)
     } return () => clearInterval(timer)
-  }, [start]);
+  }, [start])
 
   useEffect(() => {
-
-    const timeUp = 0
-
-    if (countdown && count > 0) {
-      counter = setInterval(() => {
-        setCount(previousTime => previousTime - 10)
-      }, 10)
-      //console.log(timeUp)
-    } else if (countdown && count < 0) {
+    if (count < 0) {
       clearInterval(counter)
       setCountdown(false);
       setCount(0);
-    } else {
-      clearInterval(counter)
     }
-    console.log('counter', counter) // return () => clearInterval(counter)
-  }, [countdown])
+  //console.log('counter', counter)
+  }, [countdown, count])
 
+  const timerStart = () => {
+    setCountdown(true);
+    counter = setInterval(() => {
+      setCount(previousTime => previousTime - 10)}, 10)
+  }
+  // timer stop nie dziala dla countera
+  const timerStop = () => {
+    setStart(false);
+    setCountdown(false)
+  }
 
+  const timerReset = () => {
+    setTime(0);
+    setStart(false);
+    setCount(0);
+    setCountdown(false)
+  }
 
+  const incrementHour = () => setCount(h => h + 3600000)
+
+  const incrementMminute = () => setCount(m => m + 60000)
+
+  const incrementSecound = () => setCount(s => s + 1000)
 
 
   return (
     <div>
     <div>
-      <Button action={() => setCountdown(true)}> Count </Button>
+      <Button action={() => timerStart() }> Count </Button>
       <Button action={() => setStart(true)}> Start </Button>
-      <Button action={() => {setStart(false); setCountdown(false)}}> Stop </Button>
-      <Button action={() => {setTime(0); setStart(false); setCount(0); setCountdown(false)}}> Reset </Button>
+      <Button action={() => timerStop()}> Stop </Button>
+      <Button action={() => timerReset() }> Reset </Button>
     </div>
     <div>
-      <Button action={() => setCount(h => h + 3600000)}> Hour </Button>
-      <Button action={() => setCount(m => m + 60000)}> Minute </Button>
-      <Button action={() => {setCount(s => s + 1000);}}> Secound </Button>
+      <Button action={() => incrementHour()}> Hour </Button>
+      <Button action={() => incrementMminute()}> Minute </Button>
+      <Button action={() => incrementSecound()}> Secound </Button>
     </div>
     <Time time={time} />
     <Time time={count} />
     </div>
-
   );
 
 }
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,4 +1,4 @@
-//import styles from './Button.module.scss'
+//import styles from './Button.module.scss';
 
 const Button = props =>
   <button onClick = {props.action}>{props.children}</button>
